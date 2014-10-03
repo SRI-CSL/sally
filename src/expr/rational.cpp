847f4b08a23abad32ee449d99af22142505fac5a
Dejan Jovanovic <dejan.jovanovic@sri.com>
2014-10-03 15:42:18 -0700

    ...

---
+++ src/expr/rational.cpp
@@ -0,0 +1,16 @@
+/*
+ * rational.cpp
+ *
+ *  Created on: Oct 3, 2014
+ *      Author: dejan
+ */
+
+#include "expr/rational.h"
+
+#include "utils/ostream.h"
+
+using namespace std;
+using namespace sal2;
+using namespace term;
+
+
---
+++ src/expr/rational.h
@@ -0,0 +1,31 @@
+/*
+ * rational.h
+ *
+ *  Created on: Oct 3, 2014
+ *      Author: dejan
+ */
+
+#pragma once
+
+#include <gmp.h>
+#include <string>
+#include <iostream>
+
+namespace sal2 {
+namespace term {
+
+class rational {
+  mpq_class d_gmp_rat;
+public:
+  rational(): d_gmp_rat(0) { d_gmp_rat.canonicalize(); }
+  rational(const mpq_class& gmp_rat) : d_gmp(gmp_rat) {}
+  rational(const char* s): d_gmp_rat(s, 10) { d_gmp_rat.canonicalize(); }
+  rational(std::string s): d_gmp_rat(s, 10) { d_gmp_rat.canonicalize(); }
+  rational(const rational& q): d_gmp_rat(q.d_gmp_rat) { d_gmp_rat.canonicalize(); }
+
+  std::string to_string() const;
+  void to_stream(std::ostream& out) const;
+};
+
+}
+}
