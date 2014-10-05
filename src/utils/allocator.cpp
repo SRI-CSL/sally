76d41beeab976bcc787dd69d51273598e49cc095
Dejan Jovanovic <dejan.jovanovic@gmail.com>
2014-10-04 17:09:08 -0700

    ...

---
+++ src/utils/allocator.cpp
@@ -0,0 +1,14 @@
+/*
+ * allocator.c
+ *
+ *  Created on: Oct 4, 2014
+ *      Author: dejan
+ */
+
+#include "utils/allocator.h"
+
+using namespace sal2;
+using namespace alloc;
+
+const allocator_base::ref allocator_base::ref::null;
+
