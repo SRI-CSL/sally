49199d04b07927561d37f0f361079ff7f576c62a
Dejan Jovanovic <dejan.jovanovic@sri.com>
2014-10-24 18:54:16 -0700

    ...

--- cmake/FindYices2.cmake
+++ cmake/FindYices2.cmake
@@ -4,13 +4,22 @@
 #  YICES2_INCLUDE_DIRS - The yices2 include directories
 #  YICES2_LIBRARIES - The libraries needed to use yices2
 
-find_path(YICES2_INCLUDE_DIR yices.h PATHS ENV YICES_HOME)
-find_library(YICES2_LIBRARY libyices PATHS ENV YICES_HOME)
+if (YICES2_HOME)
+  find_path(YICES2_INCLUDE_DIR yices.h PATHS "${YICES2_HOME}/include")
+else() 
+  find_path(YICES2_INCLUDE_DIR yices.h)
+endif()
+
+if (YICES2_HOME)
+  find_library(YICES2_LIBRARY libyices PATHS "${YICES2_HOME}/lib")
+else() 
+  find_library(YICES2_LIBRARY libyices)
+endif()
 
 set(YICES2_LIBRARIES ${YICES2_LIBRARY})
 set(YICES2_INCLUDE_DIRS ${YICES2_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibYices2 DEFAULT_MSG YICES2_LIBRARY YICES2_INCLUDE_DIR)
+find_package_handle_standard_args(libyices2 DEFAULT_MSG YICES2_LIBRARY YICES2_INCLUDE_DIR)
 
 mark_as_advanced(YICES2_INCLUDE_DIR YICES2_LIBRARY)
