90185ffa605e21606d4a2cf9d7c5b07a3eff0e9f
Bruno Dutertre <bruno@csl.sri.com>
2015-01-14 10:21:17 -0800

    boost version issue + fixed TRACE macro to avoid compilation warnings

--- src/utils/trace.h
+++ src/utils/trace.h
@@ -14,7 +14,7 @@
 //
 
 #ifdef NDEBUG
-#define TRACE(tag) false && std::cerr
+#define TRACE(tag) if (false) std::cerr
 #else
-#define TRACE(tag) sal2::output::trace_tag_is_enabled(tag) && sal2::output::get_trace_stream()
+#define TRACE(tag) if (sal2::output::trace_tag_is_enabled(tag)) sal2::output::get_trace_stream()
 #endif
