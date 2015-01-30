6cd403122cfcbcf0e90edda6b3400857775b7f51
Dejan Jovanovic <dejan.jovanovic@sri.com>
2015-01-30 10:43:17 -0800

    fixed the encoding, the stricts are now invalid

--- examples/tte_synchro/run_all.sh
+++ examples/tte_synchro/run_all.sh
@@ -2,5 +2,5 @@
 for file in *.*.mcmt; 
 do 
   echo $file; 
-  ../../build/src/sally --lsal-extensions $@ tte_synchro.mcmt $file; 
+  /usr/bin/time -f "%U" ../../build/src/sally --lsal-extensions $@ tte_synchro.mcmt $file; 
 done;
