6e194bf8bc6d746bf626d1d9dbee14caf32b6a66
Dejan Jovanovic <dejan.jovanovic@gmail.com>
2015-03-15 20:17:11 -0700

    trying tta for n = 3

--- examples/tta_startup/get_mcmt_from_sal.sh
+++ examples/tta_startup/get_mcmt_from_sal.sh
@@ -3,7 +3,7 @@
 INPUT=$1
 PROPERTY=$2
 
-$HOME/Software/sal-3.3/bin/salenv-safe <<EOF | tail -n +12 | head -n -1 > $INPUT.flat 
+$HOME/Software/sal-3.3/bin/salenv-safe <<EOF | tail -n +12 | head -n -1 | sed s/'AND'/'and'/g | sed s/'if'/'ite'/g | sed s/'OR'/'or'/g > $INPUT.flat 
  (sal/set-pp-max-width! 10000)
  (sal/set-pp-max-depth! 10000)
  (sal/set-pp-max-num-lines! 100000)
