a7ca897bcf76fd5bbdfc80bd59e08f6ac5998000
Dejan Jovanovic <dejan.jovanovic@sri.com>
2015-10-06 10:57:08 -0700

    removing sudo

--- contrib/install_yices2.sh
+++ contrib/install_yices2.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -d "$HOME/yices2/lib" ]; then
   wget "http://yices.csl.sri.com/cgi-bin/yices2-newnewdownload.cgi?file=yices-2.3.1-src.tar.gz&accept=I+Agree" -O yices.tar.gz
   tar xzvf yices.tar.gz
-  cd yices* && autoconf && ./configure --prefix=$HOME/yices2 && make MODE=release && sudo make MODE=release install && cd ..
+  cd yices* && autoconf && ./configure --prefix=$HOME/yices2 && make MODE=release && make MODE=release install && cd ..
 else
   echo 'Using cached directory.';
 fi
