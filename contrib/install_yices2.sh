8515d61af5ea1b23ff79d46cb252d4fadc26a0cc
Dejan Jovanovic <dejan.jovanovic@sri.com>
2015-10-06 10:54:10 -0700

    fiddling with travis

---
+++ contrib/install_yices2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e 
+
+# Check if the yices2 folder is empty
+if [ ! -d "$HOME/yices2/lib" ]; then
+  wget "http://yices.csl.sri.com/cgi-bin/yices2-newnewdownload.cgi?file=yices-2.3.1-src.tar.gz&accept=I+Agree" -O yices.tar.gz
+  tar xzvf yices.tar.gz
+  cd yices* && autoconf && ./configure --prefix=$HOME/yices2 && make MODE=release && sudo make MODE=release install && cd ..
+else
+  echo 'Using cached directory.';
+fi
