1dfdfb74276e72965f8b49fab931621bc8a3681f
Dejan Jovanovic <dejan.jovanovic@sri.com>
2016-04-05 14:54:23 -0700

    more aiger stuff

--- src/parser/aiger/aiger.cpp
+++ src/parser/aiger/aiger.cpp
@@ -43,6 +43,9 @@ class aiger_parser : public internal_parser_interface {
   /** Type of aiger ids */
   typedef unsigned aiger_id_type;
 
+  /** Aiger data */
+  aiger* d_aiger;
+
   /** Map from terms to aiger ids */
   typedef boost::unordered_map<expr::term_ref, aiger_id_type, expr::term_ref_hasher> term_to_aiger_map;
   /** Map from aiger ids to terms */
@@ -55,10 +58,10 @@ class aiger_parser : public internal_parser_interface {
   term_to_aiger_map d_term_to_aiger_map;
 
   /** Set the cache t_aiger -> t (for both negated and not negated) */
-  void set_cache(const aiger_symbol* t_aiger, expr::term_ref t);
+  void set_cache(aiger_id_type t_aiger, expr::term_ref t);
 
   /** Convert aiger to term while updating the cache */
-  expr::term_ref aiger_to_term(const aiger_symbol* t_aiger);
+  expr::term_ref aiger_to_term(aiger_id_type t_aiger);
 
 public:
 
@@ -82,24 +85,54 @@ expr::term_ref negate(expr::term_manager& tm, expr::term_ref t) {
   }
 }
 
-void aiger_parser::set_cache(const aiger_symbol* t_aiger, expr::term_ref t) {
-  assert(d_aiger_to_term_map.find(t_aiger->lit) == d_aiger_to_term_map.end());
+void aiger_parser::set_cache(aiger_id_type t_aiger, expr::term_ref t) {
+  assert(d_aiger_to_term_map.find(t_aiger) == d_aiger_to_term_map.end());
   assert(d_term_to_aiger_map.find(t) == d_term_to_aiger_map.end());
 
   // Cache negated t also
   expr::term_ref t_not = negate(d_tm, t);
 
   // aiger -> term
-  d_aiger_to_term_map[t_aiger->lit] = t;
-  d_aiger_to_term_map[aiger_not(t_aiger->lit)] = t_not;
+  d_aiger_to_term_map[t_aiger] = t;
+  d_aiger_to_term_map[aiger_not(t_aiger)] = t_not;
 
   // term -> aiger
-  d_term_to_aiger_map[t] = t_aiger->lit;
-  d_term_to_aiger_map[t_not] = aiger_not(t_aiger->lit);
+  d_term_to_aiger_map[t] = t_aiger;
+  d_term_to_aiger_map[t_not] = aiger_not(t_aiger);
 }
 
-expr::term_ref aiger_parser::aiger_to_term(const aiger_symbol* t_aiger) {
-  return expr::term_ref();
+expr::term_ref aiger_parser::aiger_to_term(aiger_id_type t_aiger) {
+
+  // Check in the cache
+  aiger_to_term_map::const_iterator find = d_aiger_to_term_map.find(t_aiger);
+  if (find != d_aiger_to_term_map.end()) {
+    return find->second;
+  }
+
+  aiger_id_type t_aiger_unsigned = aiger_strip(t_aiger);
+
+  // Variables already in cache
+  assert(aiger_is_input(d_aiger, t_aiger_unsigned) == 0);
+  assert(aiger_is_latch(d_aiger, t_aiger_unsigned) == 0);
+
+  // It's (t1 and t2), make it
+  aiger_and* t_and = aiger_is_and(d_aiger, t_aiger_unsigned);
+  assert(t_and->lhs == t_aiger_unsigned);
+
+  // Make the term
+  expr::term_ref t1 = aiger_to_term(t_and->rhs0);
+  expr::term_ref t2 = aiger_to_term(t_and->rhs1);
+  expr::term_ref t_unsigned = d_tm.mk_term(expr::TERM_AND, t1, t2);
+
+  // Set the cache
+  set_cache(t_aiger_unsigned, t_unsigned);
+
+  // Return the right value
+  if (aiger_sign(t_aiger)) {
+    return d_tm.mk_term(expr::TERM_NOT, t_unsigned);
+  } else {
+    return t_unsigned;
+  }
 }
 
 
@@ -114,6 +147,15 @@ aiger_parser::aiger_parser(const system::context& ctx, const char* filename)
   if (error != 0) {
     throw parser_exception(error);
   }
+  d_aiger = a;
+
+  // Add true and false to the cache
+  expr::term_ref t_true = d_tm.mk_boolean_constant(true);
+  expr::term_ref t_false = d_tm.mk_boolean_constant(false);
+  d_aiger_to_term_map[aiger_true] = t_true;
+  d_aiger_to_term_map[aiger_false] = t_false;
+  d_term_to_aiger_map[t_true] = aiger_true;
+  d_term_to_aiger_map[t_false] = aiger_false;
 
   // The commands will be put here
   sequence_command* all_commands = new sequence_command();
@@ -165,15 +207,50 @@ aiger_parser::aiger_parser(const system::context& ctx, const char* filename)
 
   // Add to cache
   for (size_t i = 0; i < a->num_inputs; ++ i) {
-    set_cache(a->inputs + i, input_vars[i]);
+    set_cache(a->inputs[i].lit, input_vars[i]);
   }
   for (size_t i = 0; i < a->num_latches; ++ i) {
-    set_cache(a->latches + i, state_vars[i]);
+    set_cache(a->latches[i].lit, state_vars[i]);
   }
 
+  // We have all variables and constants in the cache now. We can construct
+  // formulas.
+
+  // Get the initial states and transition formula
+  std::vector<expr::term_ref> initial_state_formulas;
+  std::vector<expr::term_ref> transition_formulas;
+  for (size_t i = 0; i < a->num_latches; ++ i) {
+    // Reset = init
+    initial_state_formulas.push_back(aiger_to_term(a->latches[i].reset));
+    // Transition
+    assert(aiger_sign(a->latches[i].lit) == 0);
+    expr::term_ref var = aiger_to_term(a->latches[i].lit);
+    expr::term_ref next_value = aiger_to_term(a->latches[i].next);
+    expr::term_ref next_var = state_type->change_formula_vars(system::state_type::STATE_CURRENT, system::state_type::STATE_NEXT, var);
+    expr::term_ref eq = d_tm.mk_term(expr::TERM_EQ, next_var, next_value);
+    transition_formulas.push_back(eq);
+  }
+
+  // Define initial states
+  expr::term_ref initial_state = d_tm.mk_and(initial_state_formulas);
+  system::state_formula* initial_state_formula = new system::state_formula(d_tm, state_type, initial_state);
+  command* define_initial = new define_states_command("aiger_init", initial_state_formula);
+  all_commands->push_back(define_initial);
+
+  // Define transition
+  expr::term_ref transition = d_tm.mk_and(transition_formulas);
+  system::transition_formula* transition_formula = new system::transition_formula(d_tm, state_type, transition);
+  command* define_transition = new define_transition_command("aiger_transition", transition_formula);
+  all_commands->push_back(define_transition);
+
+  // Define system
+  system::transition_system* aiger_system = new system::transition_system(state_type, initial_state_formula, transition_formula);
+  command* define_system = new define_transition_system_command("aiger_system", aiger_system);
+  all_commands->push_back(define_system);
+  
   // Get the properties
   for (size_t i = 0; i < a->num_outputs; ++ i) {
-    expr::term_ref p_i = aiger_to_term(a->outputs + i);
+    expr::term_ref p_i = aiger_to_term(a->outputs[i].lit);
     system::state_formula *p = new system::state_formula(d_tm, state_type, p_i);
     command* query = new query_command(ctx, "aiger_system", p);
     all_commands->push_back(query);
