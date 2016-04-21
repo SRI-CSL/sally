2b7d78a5f28d6c46ff2b40c28bb2e235055eb5cb
Lucas Baudin <lucas.baudin@ens.fr>
2016-04-21 01:20:54 +0000

    [frontend] add a pass to the simplifier to factorize existential quantifier

--- frontend/ast/lispy_simplifier.ml
+++ frontend/ast/lispy_simplifier.ml
@@ -31,7 +31,71 @@ let identity_pass = function
 	| Not(Not(b)) -> b
 	| a -> a
 
-let simplify_condition = apply_to_condition identity_pass
+(** A first binded existential variable is a variable which is bound at the top
+	of the condition tree (or that has only existential parents) *)
+let rec get_first_exist_binded_variables = function
+	| Exists(a, t, c) -> (a,t) :: get_first_exist_binded_variables c
+	| a -> []
+
+let rec list_inter a b compare =
+	let a = List.sort compare a
+	and b = List.sort compare b in
+	let rec list_inter_aux = function
+	| (t1::q1, t2::q2) ->
+		let comp = compare t1 t2 in
+		if comp > 0 then
+			list_inter_aux (q1, (t2::q2))
+		else if comp < 0 then
+			list_inter_aux ((t1::q1), q2)
+		else (t1, t2) :: (list_inter_aux (q1, q2))
+	| ([], _) -> []
+	| (_, []) -> []
+	in
+	list_inter_aux (a, b)
+
+let rec remove_first_existentials b l =
+	match b with
+	| Exists(n, t, c) ->
+		let c = remove_first_existentials c l in
+		if List.exists (fun ((name, _), _) -> name = n) l then
+			c
+		else
+			Exists(n, t, c)
+	| a ->
+		let renaming = List.fold_left (fun p ((name, _), (subst_name, _)) ->
+			p >> begin function
+			| Ident(s, mytype) when s = name -> Ident(subst_name, mytype)
+			| a -> a
+			end
+		) (fun x -> x) l in
+		apply_to_condition renaming a
+
+let existential_pass = function
+	| Or(a, b) ->
+		(* This code is not optimal, let's keep in mind that the existential
+		   variable list has probably a length < 4 (anyway, if it is too long
+		   there will be a huge bottleneck in the solver), so we can have a few
+		   extra split, combine, sort on those lists *)
+		let a_variables = get_first_exist_binded_variables a
+		and b_variables = get_first_exist_binded_variables b
+		in
+		let inter_a, inter_b =
+			list_inter a_variables b_variables ( fun a b ->
+				String.compare (fst a) (fst b))
+			|> List.split
+		in
+		let a =
+			List.combine inter_a inter_b
+			|> remove_first_existentials b in
+		let b =
+			List.combine inter_b inter_b
+			|> remove_first_existentials b
+		in
+		List.fold_left (fun c (var, var_type) ->
+			Exists(var, var_type, c)) (Or(a, b)) inter_b
+	| a -> a
+
+let simplify_condition = apply_to_condition (identity_pass >> existential_pass)
 
 let simplify_transition t =
 	{ t with formula = simplify_condition t.formula; }
