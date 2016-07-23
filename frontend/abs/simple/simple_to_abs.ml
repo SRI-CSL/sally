6802ca7647ca70f83d91441f5d17940024e59aa5
L Pick <lmpick@zoho.com>
2016-07-22 18:13:40 -0700

    Different approach

--- frontend/abs/simple/simple_to_abs.ml
+++ frontend/abs/simple/simple_to_abs.ml
@@ -47,75 +47,63 @@ let rec from_expr man env = function
   | Simple_ast.False -> False;;
 *)
 
-type 'a abs =
-  | Id of Var.t
-  | Num of Coeff.t
-  | Abs of 'a Abstract1.t;;
-
-(* convert from abs type to Texpr1.expr type *)
-let abs_to_texpr_expr v = function
-  | Id var -> Texpr1.Var var
-  | Num cst -> Texpr1.Cst cst
-  | _ -> Texpr1.Var v;;
-
-let rec arith_to_texpr man env v is_int binop e1 e2 =
+let rec arith_to_texpr man ctx v is_int binop e1 e2 =
   let v1 = Var.of_string ((Var.to_string v)^"_l") in
   let v2 = Var.of_string ((Var.to_string v)^"_r") in
   (* Solve branches recursively *)
-  let abs_e1 = from_expr man env v1 e1 in
-  let abs_e2 = from_expr man env v2 e2 in
-  let abs =
-    match (abs_e1, abs_e2) with
-    | (Abs abs1, Abs abs2) -> Abstract1.unify man abs1 abs2
-    | (Abs abs1, _) -> abs1
-    | (_, Abs abs2) -> abs2
-    | (_, _) -> Abstract1.top man env in
-  let env' =
+  let abs_e1 = from_expr man ctx v1 e1 in
+  let abs_e2 = from_expr man ctx v2 e2 in
+  let abs = Abstract1.unify man abs_e1 abs_e2 in
+  let env =
     if is_int
     then Environment.add (abs.Abstract1.env) [|v|] [||]
     else Environment.add (abs.Abstract1.env) [||] [|v|] in
-  let abs = Abstract1.change_environment man abs env' false in
+  let abs = Abstract1.change_environment man abs env false in
   let typ = (* more options exist *)
     if is_int
     then Texpr1.Int
     else Texpr1.Real in
   (* The expression for applying the operator - need to resolve rounding *)
   let texpr =
-    let te1 = abs_to_texpr_expr v1 abs_e1 in
-    let te2 = abs_to_texpr_expr v2 abs_e2 in
     Texpr1.of_expr (abs.Abstract1.env)
-      (Texpr1.Binop (binop, te1, te2, typ, Texpr1.Rnd)) in
+      (Texpr1.Binop (binop, Texpr1.Var v1, Texpr1.Var v2, typ, Texpr1.Rnd)) in
   (* The new environment *)
   (abs, texpr)
 and
-arith_from_expr man env v is_int binop e1 e2 =
-  let (abs, texpr) = arith_to_texpr man env v is_int binop e1 e2 in
+arith_from_expr man ctx v is_int binop e1 e2 =
+  let (abs, texpr) = arith_to_texpr man ctx v is_int binop e1 e2 in
   (* The assignment of the variable v to the texpr in the new environment *)
-  Abs (Abstract1.assign_texpr man abs v texpr None)
+  Abstract1.assign_texpr man abs v texpr None
 and
-comp_from_expr man env v typ e1 e2 =
+comp_from_expr man ctx v typ e1 e2 =
   (* get e1 - e2 *)
-  let (abs, texpr) = arith_to_texpr man env v false Texpr1.Sub e1 e2 in
+  let (abs, texpr) = arith_to_texpr man ctx v false Texpr1.Sub e1 e2 in
   (* now make e1 - e2 typ 0 constraint *)
   let tcons = Tcons1.make texpr typ in
   let tcons_arr = Tcons1.array_make abs.Abstract1.env 1 in
   Tcons1.array_set tcons_arr 0 tcons;
-  Abs (Abstract1.change_environment man (Abstract1.meet_tcons_array man abs tcons_arr) env false)
+  Abstract1.meet_tcons_array man abs tcons_arr
+(*
+  Abstract1.change_environment man (Abstract1.meet_tcons_array man abs tcons_arr) (ctx.Abstract1.env) false*)
 and
-from_expr man env v = function
-  | Nat n -> Num (Coeff.Scalar (Scalar.of_int n))
-  | Int i -> Num (Coeff.Scalar (Scalar.of_int i))
-  | Float f -> Num (Coeff.Scalar (Scalar.of_float f))
-  | Ident e -> Id (Var.of_string (e))
-  | Add (e1, e2) -> arith_from_expr man env v false Texpr1.Add e1 e2
-  | Sub (e1, e2) -> arith_from_expr man env v false Texpr1.Sub e1 e2
-  | Mul (e1, e2) -> arith_from_expr man env v false Texpr1.Mul e1 e2
-  | Div (e1, e2) -> arith_from_expr man env v false Texpr1.Div e1 e2
-  | Ge (e1, e2) -> comp_from_expr man env v Tcons1.SUPEQ e1 e2
-  | Gt (e1, e2) -> comp_from_expr man env v Tcons1.SUP e1 e2
-  | Eq (e1, e2) -> comp_from_expr man env v Tcons1.EQ e1 e2
-  | Neq (e1, e2) -> comp_from_expr man env v Tcons1.DISEQ e1 e2
+from_expr man ctx v = function
+  | Nat n ->
+      let env = Environment.add (ctx.Abstract1.env) [|v|] [||] in
+      Abstract1.change_environment_with man ctx env false;
+      Abstract1.assign_texpr man ctx v (Texpr1.of_expr (ctx.Abstract1.env) (Texpr1.Cst (Coeff.Scalar (Scalar.of_int n)))) None
+  | Int i -> Abstract1.assign_texpr man ctx v (Texpr1.of_expr (ctx.Abstract1.env) (Texpr1.Cst (Coeff.Scalar (Scalar.of_int i)))) None
+  | Float f -> Abstract1.assign_texpr man ctx v (Texpr1.of_expr (ctx.Abstract1.env) (Texpr1.Cst (Coeff.Scalar (Scalar.of_float f)))) None
+  | Ident e -> Abstract1.assign_texpr man ctx v (Texpr1.of_expr (ctx.Abstract1.env) (Texpr1.Var (Var.of_string e))) None
+  | Add (e1, e2) -> arith_from_expr man ctx v false Texpr1.Add e1 e2
+  | Sub (e1, e2) -> arith_from_expr man ctx v false Texpr1.Sub e1 e2
+  | Mul (e1, e2) -> arith_from_expr man ctx v false Texpr1.Mul e1 e2
+  | Div (e1, e2) -> arith_from_expr man ctx v false Texpr1.Div e1 e2
+  | Ge (e1, e2) -> comp_from_expr man ctx v Tcons1.SUPEQ e1 e2
+  | Gt (e1, e2) -> comp_from_expr man ctx v Tcons1.SUP e1 e2
+  | Eq (e1, e2) -> comp_from_expr man ctx v Tcons1.EQ e1 e2
+  | Neq (e1, e2) -> comp_from_expr man ctx v Tcons1.DISEQ e1 e2
   | Not e -> raise (Unimplemented "Not")
+(*
   | And (e1, e2) ->
       let v_str = Var.to_string v in
       (match (from_expr man env (Var.of_string (v_str^"_l")) e1, from_expr man env (Var.of_string(v_str^"_r")) e2) with
@@ -126,6 +114,7 @@ from_expr man env v = function
       (match (from_expr man env (Var.of_string (v_str^"_l")) e1, from_expr man env (Var.of_string(v_str^"_r")) e2) with
       | (Abs a1, Abs a2) -> Abs (Abstract1.join man a1 a2)
       | _ -> raise (Unimplemented "booleans"))
+*)
 (*
   | Cond (e1, e2, e3) ->
       (match from_expr man env (Var.of_string (v_str^"if")) e1 with
@@ -148,5 +137,5 @@ from_expr man env v = function
 let simple_to_abs man p =
   let (env, invs') = from_decls p.Simple_ast.decls in
   let invs = invs' in
-  let exprs = List.map (from_expr man env (Var.of_string "tmp")) p.exprs in
+  let exprs = List.map (from_expr man (Abstract1.top man env) (Var.of_string "tmp")) p.exprs in
   List.hd exprs;;
