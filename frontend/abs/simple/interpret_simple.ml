f0a4b9c3ba8129acf950cba3f26fed6270d20548
L Pick <lmpick@zoho.com>
2016-07-28 15:08:43 -0700

    Simple interpreter

--- frontend/abs/simple/interpret_simple.ml
+++ frontend/abs/simple/interpret_simple.ml
@@ -77,6 +77,7 @@ let rec interpret man env cond ctx = function
   | Seq (e::es) ->
       let ctx' = interpret man env cond ctx e in
       interpret man env cond ctx' (Seq es)
+  | Seq [] -> ctx
   | Cond (e1, e2, e3) ->
       let e1' = Expr1.Bool.of_expr (make_expr1 env cond e1) in
       if Expr1.Bool.is_true cond e1'
@@ -84,19 +85,38 @@ let rec interpret man env cond ctx = function
       else if Expr1.Bool.is_false cond e1'
       then interpret man env cond ctx e3
       else
-        let ctx1 = interpret man env cond ctx e2 in
+        let cond' = Cond.copy cond in
+        let ctx' = Domain1.meet_condition man cond' ctx e1' in
+        let ctx1 = interpret man env cond ctx' e2 in
         let ctx2 = interpret man env cond ctx e3 in
-        Domain1.meet man ctx1 ctx2
+        Domain1.join man ctx1 ctx2
   | _ -> raise Unexpected_expression;;
-      
-let _ =
-  let cudd = Cudd.Man.make_v () in
-  let env = Env.make Env.string_symbol cudd in
-  let env = Env.add_vars env [ ("value", `Real); ("bool", `Bool)] in
+     
+let convert_decls ds invs =
+  let rec generate pairs constraints = function
+    | [] -> (pairs, constraints)
+    | (Nat_decl str)::ds -> generate ((str, `Int)::pairs) (Ge (Ident str, Nat 0)::constraints) ds
+    | (Int_decl str)::ds -> generate ((str, `Int)::pairs) constraints ds
+    | (Real_decl str)::ds -> generate ((str, `Real)::pairs) constraints ds
+    | (Bool_decl str)::ds -> generate ((str, `Bool)::pairs) constraints ds in
+  let (pairs, constraints) = generate [] [] ds in
+  let cudd = Cudd.Man.make_v () in (* in the future, may need to change cudd settings *)
+  let env = Env.add_vars (Env.make Env.string_symbol cudd) pairs in
   let cond = Cond.make Env.string_symbol cudd in
   let apron = Polka.manager_alloc_loose() in
   let man = Domain1.man_of_mtbdd (Domain1.make_mtbdd apron) in
   let abs = Domain1.top man env in
+  let constraints = List.map (fun x -> Expr1.Bool.of_expr (make_expr1 env cond x)) (constraints @ invs) in
+  (man, env, cond, List.fold_left (Domain1.meet_condition man cond) abs constraints);;
+
+let interpret_program p =
+  let (man, env, cond, ctx) = convert_decls p.decls p.invs in
+  let res = interpret man env cond ctx p.expr in
+  printf "res:%a@." (Domain1.print man) res;;
+   
+let _ =
   let test_prog =
-    Assign (Ident "value", Cond (Cond (True, True, False), Nat 3, Nat 4)) in
-  printf "abs:%a@." (Domain1.print man) (interpret man env cond abs test_prog);;
+    { decls = [Nat_decl "x"; Int_decl "y"; Bool_decl "b"];
+      invs  = [];
+      expr  = Seq [Cond (Eq (Ident "x", Nat 0), Assign(Ident "y", Ident "x"), Assign(Ident "y", Nat 0))] } in
+  interpret_program test_prog;;
