4baf36a669f80a12183a8e43989fff03e621c5b7
L Pick <lmpick@zoho.com>
2016-08-18 20:33:56 -0700

    Change fixed point check in mcmt_learn

--- frontend/mcmt_learn.ml
+++ frontend/mcmt_learn.ml
@@ -24,7 +24,6 @@ let eval_step man env inv cond ts ctx =
     | _ -> raise Malformed_mcmt_prog in
   let current = List.map string_from_decl ts.current_sv_decls in
   let next = List.map string_from_decl ts.next_sv_decls in
-  printf "ctx: %a@." (Domain1.print man) ctx;
   let next_exprs = List.map (Expr1.var env cond) next in
   let assigned = Domain1.assign_lexpr man cond ctx current next_exprs None in
   let forget = Domain1.forget_list man assigned next in
@@ -37,34 +36,42 @@ let eval_step man env inv cond ts ctx =
     move forward one step in the transition relation;
     apply the invariants [inv] to the new domain; and
     decrement [cnt]. When [cnt = 0], widening and narrowing are performed. *)
-let rec eval_mcmt man env cond inv ctx ts cnt =
-  let ctx' = interpret false man env cond inv ctx ts.trans |> eval_step man env inv cond ts |> Domain1.join man ctx in
+let rec eval_mcmt man env cond inv init ctx ts cnt =
+  let ctx' = interpret false man env cond inv ctx ts.trans |> eval_step man env inv cond ts |>
+             (match init with
+              | None -> Domain1.join man ctx
+              | Some i -> Domain1.join man i) in
+  printf "ctx: %a@." (Domain1.print man) ctx;
   printf "ctx': %a@." (Domain1.print man) ctx';
-  if (Domain1.is_eq man ctx ctx')
-  then ctx
+  if (Domain1.is_leq man ctx' ctx)
+  then ctx'
   else if cnt = 0 then
     let widened = Domain1.widening man ctx ctx' in
-    eval_step man env inv cond ts (interpret true man env cond inv widened ts.trans) |> Domain1.join man ctx
-  else eval_mcmt man env cond inv ctx' ts (cnt - 1);;
+    printf "widened: %a@." (Domain1.print man) widened;
+    eval_mcmt man env cond inv init widened ts 0
+  else eval_mcmt man env cond inv init ctx' ts (cnt - 1);;
 
 (** Find invariants for a transition system in an mcmt program:
-      [ eval_mcmt_prog cond_size iter ts ]
+      [ eval_mcmt_prog cond_size iter use_init ts ]
     finds invariants for transition system [ts] by doing abstract interpretation
-  for [iter] iterations, where [cond_size] decides
-    the initial size for the condition BDD. If [cond_size] is too small,
+    for [iter] iterations, where [cond_size] decides
+    the initial size for the condition BDD and [use_init] determines whether
+    the result from taking one step of the transition is joined with the
+    initial state or the previous state. If [cond_size] is too small,
     its size is doubled, and [eval_mcmt_prog] begins again. *)
-let rec eval_mcmt_prog cond_size iter ts =
+let rec eval_mcmt_prog cond_size iter use_init ts =
   let decls = ts.decls @ ts.current_sv_decls @ ts.next_sv_decls in
   let invs = ts.invs in
   let apron_man = Polka.manager_alloc_strict() in
   try (
-    let (man, env, cond, ctx) = initialize apron_man decls invs cond_size in
-    printf "invariant: %a@." (Domain1.print man) ctx;
-    let init = interpret true man env cond ctx ctx ts.init in
+    let (man, env, cond, invariant) = initialize apron_man decls invs cond_size in
+    printf "invariant: %a@." (Domain1.print man) invariant;
+    let init = interpret true man env cond invariant invariant ts.init in
     printf "initial state: %a@." (Domain1.print man) init;
-    let res = eval_mcmt man env cond ctx init ts iter in
+    let init' = match use_init with true -> Some init | false -> None in
+    let res = eval_mcmt man env cond invariant init' init ts iter in
     mcmt_of_domain ts.name man apron_man env res)
-  with Bdd.Env.Bddindex -> eval_mcmt_prog (cond_size * 2) iter ts;;
+  with Bdd.Env.Bddindex -> eval_mcmt_prog (cond_size * 2) iter use_init ts;;
 
 (** [ print_transition_system ts ] prints the transition system [ts]
     after it has been converted into simple ASTs *)
@@ -77,9 +84,11 @@ let print_transition_system ts =
 let () =
   let input_file = ref None in
   let num_iterations = ref 10 in
+  let use_init = ref true in
   (let open Arg in
    Arg.parse [
-     "--iterations", Int (fun x -> num_iterations := x), "Number of iterations for the interpreter"
+     "--iterations", Int (fun x -> num_iterations := x), "Number of iterations for the interpreter";
+     "--use-nonstandard-transformer", Unit (fun x -> use_init := false), "Use nonstandard transformer: F(X) = X join T(X)"
    ] (fun f -> input_file := Some f)
      "Abstract interpreter for MCMT files.");
   let in_ch = create_channel_in !input_file in
@@ -87,7 +96,7 @@ let () =
   close_in in_ch;
   mcmt_to_ts parsed
   |> fun x -> List.iter print_transition_system x; x
-  |> List.map (eval_mcmt_prog 1000 !num_iterations)
+  |> List.map (eval_mcmt_prog 1000 !num_iterations !use_init)
   |> List.fold_left (fun x y -> x^"\n"^y) ""
   |> fun x -> printf "%s@." x; x
   |> add_learned !input_file;;
