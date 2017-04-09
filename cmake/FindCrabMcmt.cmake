e0624888d5eda5cec370c6241aefb37c6babc41d
caballa <navasjorgea@gmail.com>
2017-04-08 17:56:45 -0700

    Fixing more compilation/linking errors

--- cmake/FindCrabMcmt.cmake
+++ cmake/FindCrabMcmt.cmake
@@ -13,21 +13,14 @@ endif()
 
 if (CRABMCMT_HOME)
   find_library(CRABMCMT_LIBRARY crab_mcmt PATHS "${CRABMCMT_HOME}/lib" NO_DEFAULT_PATH)
-  set (CRABMCMT_LIB_PATH "${CRABMCMT_HOME}/lib")
-# else() 
-#   find_library(CRABMCMT_LIBRARY crab_mcmt NO_DEFAULT_PATH)
+  find_library(CRAB_LIBRARY Crab PATHS "${CRABMCMT_HOME}/lib" NO_DEFAULT_PATH)
 endif()
 
-## JN: a bit of a hook
-file(GLOB crabmcmt_libs "${CRABMCMT_LIB_PATH}/lib*")
-
-
-set(CRABMCMT_LIBRARIES ${CRABMCMT_LIBRARY} ${crabmcmt_libs})
+set(CRABMCMT_LIBRARIES ${CRABMCMT_LIBRARY} ${CRAB_LIBRARY})
 set(CRABMCMT_INCLUDE_DIRS ${CRABMCMT_INCLUDE_DIR})
 
-message(STATUS "crab-mcmt libraries=" ${CRABMCMT_LIBRARIES})
-
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CrabMcmt DEFAULT_MSG CRABMCMT_LIBRARY CRABMCMT_INCLUDE_DIR)
+find_package_handle_standard_args(CrabMcmt
+  DEFAULT_MSG CRABMCMT_LIBRARY CRAB_LIBRARY CRABMCMT_INCLUDE_DIR)
 
-mark_as_advanced(CRABMCMT_INCLUDE_DIR CRABMCMT_LIBRARY)
+mark_as_advanced(CRABMCMT_INCLUDE_DIR CRABMCMT_LIBRARY CRAB_LIBRARY)
