e842490a3014504b039d9b568a63f68ac2533830
Dejan Jovanovic <dejan.jovanovic@sri.com>
2017-06-23 15:25:44 -0400

    Adding option to run SMTLIB2 solver in non-incremental mode.

--- src/smt/generic/generic_solver_info.h
+++ src/smt/generic/generic_solver_info.h
@@ -19,14 +19,27 @@
 #pragma once
 
 #include "smt/generic/generic_solver.h"
+#include "smt/incremental_wrapper.h"
 
 #include <string>
 #include <boost/program_options/options_description.hpp>
+
 
 namespace sally {
 namespace smt {
 
 struct generic_solver_info {
+
+  class generic_solver_constructor : public solver_constructor {
+    expr::term_manager& d_tm;
+    const options& d_opts;
+    utils::statistics& d_stats;
+  public:
+    generic_solver_constructor(expr::term_manager& tm, const options& opts, utils::statistics& stats)
+    : d_tm(tm), d_opts(opts), d_stats(stats) {}
+    ~generic_solver_constructor() {};
+    solver* mk_solver() { return new generic_solver(d_tm, d_opts, d_stats); }
+  };
 
   static void setup_options(boost::program_options::options_description& options) {
     using namespace boost::program_options;
@@ -34,6 +47,7 @@ struct generic_solver_info {
         ("generic-solver-script", value<std::string>(), "The SMT solver script to use (takes SMT2 from stdin).")
         ("generic-solver-logic", value<std::string>(), "The SMT logic to use (e.g. QF_LRA).")
         ("generic-solver-log", value<std::string>(), "Prefix of a file where the SMT2 output will be logged. Given 'output', the files generated will be 'output.1.smt2', ...")
+        ("generic-solver-flatten", "Run the solver in non-incremental mode.")
         ;
   }
 
@@ -42,7 +56,12 @@ struct generic_solver_info {
   }
 
   static solver* new_instance(const solver_context& ctx) {
-    return new generic_solver(ctx.tm, ctx.opts, ctx.stats);
+    if (ctx.opts.get_bool("generic-solver-flatten")) {
+      solver_constructor* constructor = new generic_solver_constructor(ctx.tm, ctx.opts, ctx.stats);
+      return new incremental_wrapper("generic-solver-nonincremental", ctx.tm, ctx.opts, ctx.stats, constructor);
+    } else {
+      return new generic_solver(ctx.tm, ctx.opts, ctx.stats);
+    }
   }
 
 };
