7d5f08fd018dc873f05468e0b43cedcf41abaee7
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-04-09 14:52:23 -0400

    adding opensmt2 to travis build

---
+++ contrib/install_opensmt2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e 
+
+# opensmt2
+pushd .
+git clone https://scm.ti-edu.ch/repogit/opensmt2.git
+cd opensmt2
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DPRODUCE_PROOF=ON ..
+make 
+sudo make install
+popd 
