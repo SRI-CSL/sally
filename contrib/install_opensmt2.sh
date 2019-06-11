db527c20faca08391cbfa47cec2f3b195ccba9e0
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-06-11 10:38:23 -0400

    Revert "trying official opensmt2"
    This reverts commit afc39c2433dcca9b204c14ac43561fabc8bc3c67.

--- contrib/install_opensmt2.sh
+++ contrib/install_opensmt2.sh
@@ -3,8 +3,8 @@ set -e
 
 # opensmt2
 pushd .
-git clone https://scm.ti-edu.ch/repogit/opensmt2.git
-# git clone https://github.com/dddejan/opensmt2.git
+# git clone https://scm.ti-edu.ch/repogit/opensmt2.git
+git clone https://github.com/dddejan/opensmt2.git
 cd opensmt2
 mkdir build
 cd build
