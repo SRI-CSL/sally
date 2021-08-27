5fe290fa3e069f1f034e5f5b5efa1124b68b2e04
Martin Blicha <martin.blicha@usi.ch>
2021-08-27 17:44:19 +0200

    Update helper script for installing OpenSMT

--- contrib/install_opensmt2.sh
+++ contrib/install_opensmt2.sh
@@ -3,11 +3,11 @@ set -e
 
 # opensmt2
 pushd .
-git clone --depth 1 --branch OpenSMT-2.0.0 --single-branch https://github.com/usi-verification-and-security/opensmt.git
+git clone --depth 1 --branch v2.1.0 --single-branch https://github.com/usi-verification-and-security/opensmt.git
 cd opensmt
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DUSE_READLINE=ON -DPACKAGE_TESTS=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXECUTABLES=OFF -DPACKAGE_TESTS=OFF ..
 make 
 sudo make install
 popd
